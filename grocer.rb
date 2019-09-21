c680fabf24b6266ae1780f9aa3a1d8c5eef03b28
Phil Billingsby <paulbillingsby@gmail.com>
2019-09-21 18:32:29 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,19 @@
 def consolidate_cart(cart)
   # code here
+  new_hash = {}
+  cart.each do |item_arr| # for each item in the cart
+    item_arr.each do |k, v| # for each key and value in item of cart
+      new_hash[k] ||= v # which items are duplicated
+      new_hash[k][:count] ? new_hash[k][:count] += 1 : new_hash[k][:count] = 1
+    end
+  end
+  new_hash  
 end
 
 def apply_coupons(cart, coupons)
   # code here
+  
+  
 end
 
 def apply_clearance(cart)
